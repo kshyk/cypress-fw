bc443d9ab4f61d5cc2529ef70437db060cd60155
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2022-08-10 01:03:26 +0200

    fix: for configFile is invalid

--- cypress.config.js
+++ cypress.config.js
@@ -1,17 +1,14 @@
-import { defineConfig } from 'cypress'
+const { defineConfig } = require('cypress');
 
-export default defineConfig({
+module.exports = defineConfig({
   video: false,
   chromeWebSecurity: false,
   experimentalStudio: true,
   projectId: 'xd2d1c',
   retries: 2,
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
+    support: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
-  },
-})
+    experimentalSessionAndOrigin: true
+  }
+});
