b414af9b3706dd6466a96e65900ce8b0cf1483af
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2022-08-13 00:47:15 +0200

    updated os images

--- cypress.config.js
+++ cypress.config.js
@@ -2,9 +2,13 @@ const { defineConfig } = require('cypress');
 
 module.exports = defineConfig({
   video: false,
+  videoUploadOnPasses: false,
   chromeWebSecurity: false,
   projectId: 'xd2d1c',
-  retries: 2,
+  retries: {
+    'runMode': 1,
+    'openMode': 0
+  },
   e2e: {
     support: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
