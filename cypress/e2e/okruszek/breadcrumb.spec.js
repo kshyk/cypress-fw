d3b710a082472da61118096a2695b979b86786d6
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2022-11-19 22:18:52 +0100

    fix: security hotspot

--- cypress/e2e/okruszek/breadcrumb.spec.js
+++ cypress/e2e/okruszek/breadcrumb.spec.js
@@ -1,6 +1,6 @@
 describe("Okruszek", () => {
     it("should donate after bread click", () => {
-        cy.visit("http://www.okruszek.org.pl/");
+        cy.visit("www.okruszek.org.pl/");
         cy.get(".click-crumb").trigger('mouseover').click();
         cy.get("section#thanks", { timeout: 10000 }).should("be.visible");
     });
