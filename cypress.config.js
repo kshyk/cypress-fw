4e1f75ceb02d9346f80c2c137a6fdfb048476860
Jakub Turkiewicz <jj.turkiewicz@gmail.com>
2022-12-04 22:35:28 +0100

    feat: bump cypress version

--- cypress.config.js
+++ cypress.config.js
@@ -3,13 +3,24 @@ const { defineConfig } = require('cypress');
 module.exports = defineConfig({
   video: false,
   videoUploadOnPasses: false,
-  chromeWebSecurity: false,
   projectId: 'xd2d1c',
   retries: {
     'runMode': 1,
     'openMode': 0
   },
   e2e: {
+    setupNodeEvents(on, config) {
+      on('before:browser:launch', (browser = {}, launchOptions) => {
+        if (browser.family === 'chromium' || browser.family === 'chrome') {
+          launchOptions.args.push('--disable-infobars')
+          launchOptions.args.push('--disable-gpu')
+          launchOptions.args.push('--disable-dev-shm-usage')
+          launchOptions.args.push('--no-sandbox')
+          launchOptions.args.push('--disable-web-security')
+        }
+        return launchOptions;
+      })
+    },
     support: 'cypress/support/e2e.js',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
     experimentalSessionAndOrigin: true
