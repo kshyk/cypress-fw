35e2bbe5d7990498800aaa3d036a3fbb49d28352
Jakub Turkiewicz <jakub.turkiewicz@beekeeper.io>
2023-12-26 01:48:35 +0100

    tweak cookie bar tests

--- cypress/e2e/teamandpersonal/cookies.cy.ts
+++ cypress/e2e/teamandpersonal/cookies.cy.ts
@@ -1,18 +1,14 @@
 describe('Cookie Policy', () => {
-  beforeEach(() => {
-    cy.visit('https://teamandpersonal.pl/');
-  });
-
-  it('should appear on entry point', () => {
-    cy.get('aside#moove_gdpr_cookie_info_bar')
-      .should('be.visible')
-      .should('contain.text', 'Używamy ciasteczek, aby zapewnić najlepszą jakość korzystania z naszej witryny.')
-      .should('contain.text', 'Możesz dowiedzieć się więcej o tym, jakich ciasteczek używamy, lub wyłączyć je w ustawieniach.');
-  });
-
-  it('should disappear when dismissed by OK button', () => {
-    cy.get('aside#moove_gdpr_cookie_info_bar').as('cookieBar');
-    cy.get('.moove-gdpr-infobar-allow-all').trigger('mouseover').click();
-    cy.get('@cookieBar').should('not.be.visible');
+  it('check cookie bar content and dismiss it afterwards', () => {
+    cy.origin('https://teamandpersonal.pl', ()=> {
+      cy.visit('/');
+      cy.get('aside#moove_gdpr_cookie_info_bar')
+        .should('be.visible')
+        .as('cookieBar')
+        .should('contain.text', 'Używamy ciasteczek, aby zapewnić najlepszą jakość korzystania z naszej witryny.')
+        .should('contain.text', 'Możesz dowiedzieć się więcej o tym, jakich ciasteczek używamy, lub wyłączyć je w ustawieniach.');
+      cy.get('.moove-gdpr-infobar-allow-all').trigger('mouseover').click();
+      cy.get('@cookieBar').should('not.be.visible');
+    });
   });
 });
